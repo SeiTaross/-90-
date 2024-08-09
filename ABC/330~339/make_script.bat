b9761630d0dcece3eaab3e241b5cc67f7848d8c3
Seitaro.Inagaki <seitaro.inagaki@ivis.co.jp>
2024-08-09 17:10:10 +0900

    batファイルの修正

--- ABC/330~339/make_script.bat
+++ ABC/330~339/make_script.bat
@@ -5,4 +5,4 @@ set dest_dir=%1
 set file_name=%2
 
 rem cp コマンドを使ってファイルをコピーする
-copy python_input_template.py "ABC\%dest_dir%\%file_name%.py"
+copy python_input_template.py "%dest_dir%\%file_name%.py"
